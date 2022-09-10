b721333ec14b6f148c719bf27eecd0c962389645
Vladimir Kumalagov <95184524+KumaCrypto@users.noreply.github.com>
2022-09-10 19:49:20 +0200

    Fix async bug with metadata

--- metadata/scripts/deployMetadata.ts
+++ metadata/scripts/deployMetadata.ts
@@ -13,33 +13,29 @@ async function deployMetadata() {
 		throw new Error("DEPLOY_TO_PINATA in .env equals to false ");
 }
 
-async function deployImages(pathToImages: string) {
+async function storeImages(imagesFilePath: string) {
 	// resolve => create full path, from "./metadata/RandomDogies" to "/Users/UserName/Dir/Dir/Dir"
-	const fullImagesPath = path.resolve(pathToImages);
+	const fullImagesPath = path.resolve(imagesFilePath);
 
 	// All files from dir
 	const files = fs.readdirSync(fullImagesPath);
 	const responses: Array<any> = [];
 
-	for (const fileIndex in files) {
-		console.log(fileIndex);
-		const readeableStreamForFile = fs.createReadStream(
-			`${fullImagesPath}/${fileIndex}`
+	for (const indexFile in files) {
+		const readableStreamForFile = fs.createReadStream(
+			`${fullImagesPath}/${files[indexFile]}`
 		);
 
 		try {
-			const response = await pinata.pinFileToIPFS(readeableStreamForFile);
+			const response = await pinata.pinFileToIPFS(readableStreamForFile);
 			responses.push(response);
 		} catch (e: any) {
 			console.log(e.message);
 			process.exit(1);
 		}
 	}
-	console.log(responses, files);
+
 	return { responses, files };
 }
 
-deployImages("./metadata/RandomDogies").catch((error: any) => {
-	console.log(error);
-	process.exit(1);
-});
+export { storeImages };
